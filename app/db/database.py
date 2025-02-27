7e069db63c79c67dd8718efc41cdaf04841a4291
Gokul K R <gokul.kr@bigthinkcode.com>
2025-02-27 14:56:44 +0530

    updated database.py

--- app/db/database.py
+++ app/db/database.py
@@ -3,9 +3,9 @@ This file has the code to create a connection with the database.
 """
 import os
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from collections.abc import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from dotenv import load_dotenv
 
@@ -25,6 +25,20 @@ conn_string = (
     f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 # Create SQLAlchemy engine and session
-engine = create_async_engine(conn_string)
-SessionMaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+engine = create_async_engine(
+    conn_string,
+    future=True,
+    echo=True,
+)
+
+AsyncSessionFactory = async_sessionmaker(
+    engine, autoflush=False, expire_on_commit=False
+)
+
+
+async def get_db() -> AsyncGenerator:
+    async with AsyncSessionFactory() as session:
+        try:
+            yield session
+        except Exception as e:
+            raise
